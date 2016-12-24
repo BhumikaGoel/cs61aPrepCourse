f0a542848a7a4bbfa15f371346db2cc306361712
Bhumika Goel <bhumikagoel@Bhumikas-Air.lan>
2016-12-24 12:02:58 -0800

    Completed Exercise 5

---
+++ Chapter5/BiggerBetterNumber.rb
@@ -0,0 +1,5 @@
+puts "Hey! What is your favourite number?"
+num_s = gets.chomp
+num_i = num_s.to_i
+num_add = num_i + 1
+puts "But don't you think that " + num_add.to_s + "is a better bigger number?"
---
+++ Chapter5/FullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "Hey! What is your first name?"
+first = gets.chomp
+puts "Whats your middle name?"
+middle = gets.chomp
+puts "And what's your last name?"
+last = gets.chomp
+puts "Hi " + first + " " + middle + " " + last + "! Nice to meet you."
