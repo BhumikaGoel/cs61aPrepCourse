94ac237e0c618aaf7edba98f4c0c20476d627b4a
Bhumika Goel <bhumikagoel@Bhumikas-MacBook-Air.local>
2016-12-29 14:35:16 -0500

    Completed Exercise 7

--- Chapter7/DeafGrandmaExtended.rb
+++ Chapter7/DeafGrandmaExtended.rb
@@ -2,9 +2,13 @@ counter = 0
 while true
   answer = gets.chomp
     if answer == answer.upcase
-          if answer == 'BYE BYE BYE'
-              break
+          if answer == 'BYE'
+              counter+=1
+              if counter == 3
+                break
+              end
           else
+              counter = 0
               num = rand(21) + 1930
               puts "NO, NOT SINCE " + num.to_s + "!"
           end
