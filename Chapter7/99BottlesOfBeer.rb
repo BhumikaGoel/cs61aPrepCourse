77921f29a86537c62fdf2cc23209afd3aae31d6d
Bhumika Goel <bhumikagoel@Bhumikas-Air.lan>
2017-01-05 11:06:25 -0800

    Completed Exercise 7

--- Chapter7/99BottlesOfBeer.rb
+++ Chapter7/99BottlesOfBeer.rb
@@ -1,23 +1,25 @@
 counter = 99
 while counter>0
-  if counter != 1
-      if counter != 2
+  if counter >2
         puts counter.to_s + " bottles of beer on the wall, " + counter.to_s + " bottles of beer."
         counter-=1
         puts "Take one down and pass it around, " + counter.to_s + " bottles of beer on the wall."
-      else
+  end
+
+  if counter == 2
         puts counter.to_s + " bottles of beer on the wall, " + counter.to_s + " bottles of beer."
         counter-=1
-        puts "Take one down and pass it around, " + counter.to_s + " bottle of beer on the wall."  
-      end
-  else
+        puts "Take one down and pass it around, " + counter.to_s + " bottle of beer on the wall."
+  end
+
+  if counter == 1
       puts "1 bottle of beer on the wall, 1 bottle of beer."
       puts "Take one down and pass it around, no more bottles of beer on the wall."
       counter-=1
   end
-end
 
-if counter == 0
-  puts "No more bottles of beer on the wall, no more bottles of beer."
-  puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+  if counter == 0
+      puts "No more bottles of beer on the wall, no more bottles of beer."
+      puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+  end
 end
--- Chapter7/LeapYears.rb
+++ Chapter7/LeapYears.rb
@@ -4,7 +4,7 @@ puts "Which is the ending year?"
 final = gets.chomp.to_i
 
 while start <= final
-  if (start%100 == 0 && start%400 == 0) || (start%4 == 0 && start%100 !=0)
+  if (start%400 == 0) || (start%4 == 0 && start%100 !=0)
     puts start.to_s
   end
   start+=1
