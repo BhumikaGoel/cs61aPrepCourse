174a7cd8fdca9a6fd7573fcef199be4395de4227
Bhumika Goel <bhumikagoel@Bhumikas-Air.lan>
2017-01-12 15:57:51 -0800

    Completed Exercise 12

--- Chapter12/BirthdayFile.rb
+++ Chapter12/BirthdayFile.rb
@@ -9,7 +9,7 @@ File.read('birthdates.txt').each_line do |line|
     end
 
     name = line[0,i]
-    birthdate = line[-15 .. (-1)]
+    birthdate = line[-13 .. (-1)]
 
     birthdays[name] = birthdate
     #puts "#{name} =   #{birthdays[name]}"
@@ -23,5 +23,5 @@ ans_name = gets.chomp
 if birthdays[ans_name] == nil
   puts "Record does not exist"
 else
-  puts "#{ans_name}'s birthdate is #{birthdays[ans_name]}'"
+  puts "#{ans_name}'s birthdate is #{birthdays[ans_name]}"
 end
